60977fbf18dc123690edf806e00d93ac0338b148
Aleksandr Uzienko <a.uzienko@gmail.com>
2023-06-09 18:01:36 +0300

    fix clear bugs

--- lesson6/src/main/resources/SOLUTION.sql
+++ lesson6/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
 SELECT * FROM payment p WHERE p.type_id = (SELECT t.id FROM paymenttype t WHERE t.name = 'MONTHLY');
 SELECT * FROM mark m WHERE m.subject_id = (SELECT s.id FROM subject s WHERE s.name = 'Art');
 SELECT * FROM student s WHERE s.id IN (SELECT p.student_id FROM payment p WHERE p.type_id = (SELECT t.id FROM paymenttype t WHERE t.name = 'WEEKLY'));
-SELECT * FROM student s WHERE s.id IN (SELECT * FROM mark m WHERE m.subject_id = (SELECT s.id FROM subject s WHERE s.name = 'Math'));
+SELECT * FROM student s WHERE s.id IN (SELECT m.student_id FROM mark m WHERE m.subject_id = (SELECT s.id FROM subject s WHERE s.name = 'Math'));
